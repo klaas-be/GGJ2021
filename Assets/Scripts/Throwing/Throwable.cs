f2b6e2671e4d78665b836804ea5d4d37ef06d64b
Klaas <klaas@tinyroar.de>
2021-01-29 20:54:30 +0100

    Added throwable objects

---
+++ Assets/Scripts/Throwing/Throwable.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Throwing
+{
+    public class Throwable : MonoBehaviour
+    {
+
+        public Transform anchor;
+
+        private Transform parent;
+
+        private void Start()
+        {
+            parent = transform.parent;
+        }
+
+        /// <summary>
+        ///reattaches body part back to anchor
+        /// </summary>
+        public IEnumerator Reattach()
+        {
+
+            float elapse_time = 0;
+
+            while (Vector3.Distance(anchor.transform.position, transform.position) > 0.1f)
+            {
+                elapse_time += Time.deltaTime;
+                transform.position = Vector3.Lerp(transform.position, anchor.transform.position, elapse_time);
+                yield return null;
+            }
+            
+            transform.parent = parent;
+            transform.position = anchor.position;
+        }
+
+        /// <summary>
+        /// Unparrent the game object
+        /// </summary>
+        public void Detach()
+        {
+            parent = null;
+        }
+    }
+}
--- Assets/Scripts/Throwing/Thrower.cs
+++ Assets/Scripts/Throwing/Thrower.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Throwing;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class Thrower : MonoBehaviour
 {
@@ -9,14 +12,24 @@ public class Thrower : MonoBehaviour
     public Transform throwOrigin; 
     public Transform Target;
     public float firingAngle = 45.0f;
-    public float gravity = 9.8f;
+    public float gravityScale = 1f;
 
-    public Transform Projectile;
-    
+    public Throwable Projectile;
+
+
+    private Coroutine throwingCoroutine; 
  
     void Throw()
-    {          
-        StartCoroutine(SimulateProjectileCor());
+    {
+        Projectile.Detach();
+        if(throwingCoroutine != null)
+            StopCoroutine(throwingCoroutine);
+        throwingCoroutine = StartCoroutine(SimulateProjectileCor());
+    }
+
+    private void Unparent(Transform projectile)
+    {
+        projectile.parent = null;
     }
 
     private void Update()
@@ -25,24 +38,38 @@ public class Thrower : MonoBehaviour
         {
             Throw();
         }
+        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reset();
+        }
         ;
+    }
+
+    private void Reset()
+    {
+        if(throwingCoroutine != null)
+            StopCoroutine(throwingCoroutine);
+        
+        StartCoroutine(Projectile.Reattach());
     }
 
 
     IEnumerator SimulateProjectileCor()
     {
         // Move projectile to the position of throwing object + add some offset if needed.
-        Projectile.position = throwOrigin.position;
+        Projectile.transform.position = throwOrigin.position;
        
         // Calculate distance to target
-        float target_Distance = Vector3.Distance(Projectile.position, Target.position);
+        float target_Distance = Vector3.Distance(Projectile.transform.position, Target.position);
  
         // Calculate the velocity needed to throw the object to the target at specified angle.
-        float projectile_Velocity = target_Distance / (Mathf.Sin(2 * firingAngle * Mathf.Deg2Rad) / gravity);
+        float projectile_Velocity = target_Distance / (Mathf.Sin(2 * firingAngle * Mathf.Deg2Rad) / Physics.gravity.magnitude);
  
         
         //calculateFiringAngleIfSet
 
+        
         // Extract the X  Y componenent of the velocity
         float Vx = Mathf.Sqrt(projectile_Velocity) * Mathf.Cos(firingAngle * Mathf.Deg2Rad);
         float Vy = Mathf.Sqrt(projectile_Velocity) * Mathf.Sin(firingAngle * Mathf.Deg2Rad);
@@ -51,13 +78,13 @@ public class Thrower : MonoBehaviour
         float flightDuration = target_Distance / Vx;
    
         // Rotate projectile to face the target.
-        Projectile.rotation = Quaternion.LookRotation(Target.position - Projectile.position);
+        Projectile.transform.rotation = Quaternion.LookRotation(Target.position - Projectile.transform.position);
        
         float elapse_time = 0;
  
         while (elapse_time < flightDuration)
         {
-            Projectile.Translate(0, (Vy - (gravity * elapse_time)) * Time.deltaTime, Vx * Time.deltaTime);
+            Projectile.transform.Translate(0, (Vy - (Physics.gravity.magnitude  * elapse_time)) * Time.deltaTime, Vx * Time.deltaTime);
            
             elapse_time += Time.deltaTime;
  
