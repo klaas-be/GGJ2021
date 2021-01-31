6fc3ef47c51c9c206350c1d1fe206f27ab961737
Klaas <klaas@tinyroar.de>
2021-01-31 13:08:40 +0100

    Updated weight to work with preasureplate

--- Assets/Scripts/Weight.cs
+++ Assets/Scripts/Weight.cs
@@ -1,18 +1,47 @@
 using System;
+using System.Collections.Generic;
 using Throwing;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 namespace DefaultNamespace
 {
     public class Weight : MonoBehaviour
     {
-        private void OnTriggerEnter(Collider other)
+
+        public float influenceRadius;
+
+        private Plate pushedPlate; 
+        private void Update()
         {
-            var interactable = other.gameObject.GetComponent<Plate>();
-            if (interactable != null)
+            Collider[] touchingColliders = Physics.OverlapSphere(transform.position, influenceRadius);
+
+            bool foundPlate = false;
+            
+            foreach (var coll in touchingColliders)
             {
-                interactable.Interact();
+                var interactable = coll.gameObject.GetComponent<Plate>();
+                if (interactable != null)
+                {
+                    foundPlate = true;
+                    if (pushedPlate == null)
+                    {
+                        pushedPlate = interactable;
+                        interactable.Interact();
+                    }
+                }
+
+                if (foundPlate == false && pushedPlate !=null)
+                {
+                    pushedPlate.Interact();
+                    pushedPlate = null; 
+                }
             }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(transform.position, influenceRadius);
         }
     }
 }
